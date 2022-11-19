780c107398d50ead7458c8ae50ff8c73bd2c8b4a
Zakiroglu25 <bxtyr1903@gmail.com>
2022-11-19 16:10:38 +0400

    user ui

--- lib/presentation/page/add_address_page/fields/address_field.dart
+++ lib/presentation/page/add_address_page/fields/address_field.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uikit/utils/delegate/index.dart';
 import '../../../../../utils/constants/text.dart';
 import '../../../../../widgets/general/app_field.dart';
 import '../../../../infrastructure/cubit/add_address/add_and_update_address_cubit.dart';
+import '../../address_page/select_map_page/select_map_page.dart';
 
 class AddressField extends StatelessWidget {
   final TextEditingController? controller;
@@ -18,6 +20,13 @@ class AddressField extends StatelessWidget {
       textInputType: TextInputType.name,
       textCapitalization: TextCapitalization.sentences,
       controller: controller,
+      suffixIcon: InkWell(
+          onTap: (){
+            print("Salam");
+            // Go.to(context, MapSample());
+          },
+          child: Icon(Icons.my_location_sharp)),
+
     );
   }
 }
