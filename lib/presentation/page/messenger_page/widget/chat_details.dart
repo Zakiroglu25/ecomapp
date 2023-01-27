b477147b5d6c62a84c12e567183235dbca4914d0
Zakiroglu25 <bxtyr1903@gmail.com>
2023-01-27 16:29:13 +0400

    chat pagination qosha bilmedim esebleshdim

--- lib/presentation/page/messenger_page/widget/chat_details.dart
+++ lib/presentation/page/messenger_page/widget/chat_details.dart
@@ -17,11 +17,23 @@ class Chat extends StatelessWidget {
   Chat(this.guid, this.storeName);
 
   TextEditingController controller = TextEditingController();
-
+  final scrollController = ScrollController();
   final int messageCount = 1;
+
+  void setupScrollController(context) {
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        if (scrollController.position.pixels != 10) {
+          BlocProvider.of<ChatMessengerCubit>(context).loadMore(guid!);
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    setupScrollController(context);
+    BlocProvider.of<ChatMessengerCubit>(context).loadMore(guid!);
     return Scaffold(
       appBar: DoctorAppbar(
         title: storeName,
@@ -42,12 +54,13 @@ class Chat extends StatelessWidget {
                   children: [
                     SizedBox(
                       child: ListView.builder(
+                        controller: scrollController,
                         padding: const EdgeInsets.only(bottom: 80),
                         reverse: true,
                         shrinkWrap: false,
                         itemCount: chatList.length,
                         itemBuilder: (context, index) {
-                          return Align(
+                          return index == chatList.length ?AppLoading():Align(
                             alignment: chatList[index].isByYou == true
                                 ? Alignment.centerRight
                                 : Alignment.centerLeft,
@@ -60,11 +73,11 @@ class Chat extends StatelessWidget {
                                     const DoctoroChatLogo(),
                                   MySizedBox.w12,
                                   ChatWordsWidget(
-                                    date:chatList[index].sentAt.toString(),
+                                      date: chatList[index].sentAt.toString(),
                                       message: chatList[index].message!),
                                   MySizedBox.w12,
                                 ]),
-                          );
+                          ) ;
                         },
                       ),
                     ),
@@ -82,3 +95,109 @@ class Chat extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+//
+// import '../../../../infrastructure/cubit/chat_messages_cubit/chat_messenger_cubit.dart';
+// import '../../../../infrastructure/cubit/chat_messages_cubit/chat_messenger_state.dart';
+// import '../../../../widgets/general/app_loading.dart';
+// import 'chat_sent_field.dart';
+//
+// class Chat extends StatelessWidget {
+//   String? guid;
+//   String? storeName;
+//
+//   Chat(this.guid, this.storeName);
+//
+//   TextEditingController controller = TextEditingController();
+//
+//   final scrollController = ScrollController();
+//
+//   void setupScrollController(context) {
+//     scrollController.addListener(() {
+//       if (scrollController.position.atEdge) {
+//         if (scrollController.position.pixels != 10) {
+//           BlocProvider.of<ChatMessengerCubit>(context).loadMore(guid!);
+//         }
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     setupScrollController(context);
+//     BlocProvider.of<ChatMessengerCubit>(context).loadMore(guid!);
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       body: BlocProvider(
+//         create: (context) => ChatMessengerCubit()..fetch(context: context, guid: guid!),
+//         child: BlocBuilder<ChatMessengerCubit, ChatMessengerState>(
+//           builder: (context, state) {
+//             if (state is ChatMessengerSuccess) {
+//               var chatList = state.contactList;
+//               return Container(
+//                 height: 300,
+//                 child: ListView.builder(
+//                   controller: scrollController,
+//                   reverse: false,
+//                   shrinkWrap: true,
+//                   itemCount: chatList.length > 1 ? chatList.length:chatList.length+1,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return FadeTransition(
+//                       opacity: CurvedAnimation(
+//                           parent: const AlwaysStoppedAnimation(1),
+//                           curve: Curves.easeIn),
+//                       child: Row(
+//                         mainAxisAlignment: chatList[index].isByYou == false
+//                             ? MainAxisAlignment.start
+//                             : MainAxisAlignment.end,
+//                         children: <Widget>[
+//                           Container(
+//                             padding: const EdgeInsets.all(10),
+//                             decoration: BoxDecoration(
+//                                 color: chatList[index].isByYou == false
+//                                     ? Colors.blue[50]
+//                                     : Colors.green[50],
+//                                 borderRadius: BorderRadius.only(
+//                                     topRight: const Radius.circular(10),
+//                                     bottomLeft: const Radius.circular(10),
+//                                     bottomRight:
+//                                     chatList[index].isByYou == false
+//                                         ? const Radius.circular(10)
+//                                         : const Radius.circular(0),
+//                                     topLeft:
+//                                     chatList[index].isByYou == false
+//                                         ? const Radius.circular(0)
+//                                         : const Radius.circular(10))),
+//                             child: Text(chatList[index].message!),
+//                           ),
+//                         ],
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               );
+//             }
+//             return const Text("Salam");
+//           },
+//         ),
+//       ),
+//     );
+//     // body: BlocProvider(
+//     //   create: (context) =>
+//     //       ChatMessengerCubit()..fetch(context: context, guid: guid!),
+//     //   child: BlocBuilder<ChatMessengerCubit, ChatMessengerState>(
+//     //     builder: (context, state) {
+//     //       if (state is ChatMessengerSuccess) {
+//     //         var chatList = state.contactList;
+//     //
+//     //       } else if (state is ChatMessengerInProgress) {
+//     //         return const AppLoading();
+//     //       }
+//     //       return const AppLoading();
+//     //     },
+//     //   ),
+//     // ),
+//
+//   }
+// }
